4f3c7ac861450837c3c5354360b3ac9d450e724b
Vicente Nascimento <efenequis19981@gmail.com>
2021-04-26 21:14:16 -0300

    feat: create constants of application

---
+++ app/src/main/java/com/devicente/githubexplorer/service/constants/GithubExplorerConstants.kt
@@ -0,0 +1,26 @@
+package com.devicente.githubexplorer.service.constants
+
+class GithubExplorerConstants {
+    companion object {
+        const val BASE_URL = "https://api.github.com/"
+    }
+
+    object HTTP {
+        const val SUCCESS = 200
+    }
+
+    object BUNDLE {
+        const val GITHUB_NAME_USER = "searchname"
+        const val GITHUBFILTER = "githubfilter"
+        const val GITHUBREPOSITORY = "githubrepository"
+    }
+
+    object FILTER {
+        const val REPOS = 0
+        const val GISTS = 1
+        const val FOLLOWERS = 2
+    }
+
+
+
+}
