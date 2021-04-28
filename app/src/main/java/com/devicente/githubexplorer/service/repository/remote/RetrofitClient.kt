9f9d55e391ab5c5766847d05706d6e8842087afd
Vicente Nascimento <efenequis19981@gmail.com>
2021-04-27 22:21:55 -0300

    refactor: added mosh config builder and remove Gson

--- app/src/main/java/com/devicente/githubexplorer/service/repository/remote/RetrofitClient.kt
+++ app/src/main/java/com/devicente/githubexplorer/service/repository/remote/RetrofitClient.kt
@@ -1,11 +1,11 @@
 package com.devicente.githubexplorer.service.repository.remote
 
 import com.devicente.githubexplorer.service.constants.GithubExplorerConstants
-import okhttp3.Interceptor
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
-import okhttp3.Response
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class RetrofitClient private constructor() {
     companion object {
@@ -16,16 +16,17 @@ class RetrofitClient private constructor() {
         private fun getRetrofitInstance(): Retrofit {
             val httpClient = OkHttpClient.Builder()
             if (!Companion::retrofit.isInitialized) {
+                val mosh = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
                 retrofit = Retrofit.Builder()
                     .baseUrl(baseUrl)
                     .client(httpClient.build())
-                    .addConverterFactory(GsonConverterFactory.create())
+                    .addConverterFactory(MoshiConverterFactory.create(mosh))
                     .build()
 
             }
             return retrofit
         }
-
 
         fun <T> createService(serviceClass: Class<T>): T {
             return getRetrofitInstance().create(serviceClass)
